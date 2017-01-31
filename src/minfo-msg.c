65767846aeda7390445e52f89bd06ca614181d72
Jamie Syme <jamie.d.syme@gmail.com>
2017-01-31 06:40:24 -0800

    Added cla parsing to minfo-msg.

--- src/minfo-msg.c
+++ src/minfo-msg.c
@@ -1,11 +1,42 @@
+#include <stdio.h>
+#include <string.h>
 #include "radio-msg.h"
 #include "radio-transmitter.h"
 
-int main()
+int main(int argc, char** argv)
 {
   RadioMsg msg;
-  msg.type = RMSG_STOP;
-  sendRadioMsg(msg);
 
-  return 0;
+  if (argc < 2 ||
+      strcmp(argv[1], "-h") == 0 ||
+      strcmp(argv[1], "--help") == 0) {
+    printf("TODO: print help\n");
+    return 0;
+  }
+
+  if (strcmp(argv[1], "toggle") == 0) {
+    printf("TODO: toggle\n");
+    return 0;
+  }
+
+  if (strcmp(argv[1], "show") == 0) {
+    msg.type = RMSG_SHOW_ALL;
+    sendRadioMsg(msg);
+    return 0;
+  }
+
+  if (strcmp(argv[1], "hide") == 0) {
+    msg.type = RMSG_HIDE_ALL;
+    sendRadioMsg(msg);
+    return 0;
+  }
+
+  if (strcmp(argv[1], "stop") == 0) {
+    msg.type = RMSG_STOP;
+    sendRadioMsg(msg);
+    return 0;
+  }
+
+  printf("unknown command: %s\n", argv[1]);
+  return 1;
 }
