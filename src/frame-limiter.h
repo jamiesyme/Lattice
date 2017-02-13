52b28f95cc6ac349d5414c6af8c6dec2bc5789fe
Jamie Syme <jamie.d.syme@gmail.com>
2017-02-13 15:49:41 -0800

    Improved docs for frame limiter and time utilities.

--- src/frame-limiter.h
+++ src/frame-limiter.h
@@ -1,10 +1,14 @@
 #pragma once
 
+// A frame limiter is used to cap the framerate at a particular fps. This is
+// just to save on CPU cycles (what kind of status bar needs more than 60 fps).
+
 typedef struct FrameLimiter FrameLimiter;
 
 
 FrameLimiter* newFrameLimiter(int targetFps);
-
 void freeFrameLimiter(FrameLimiter* limiter);
 
+// Call this from the main loop once per frame. It will sleep for however long
+// is necessary to reach the target fps.
 void applyFrameLimiter(FrameLimiter* limiter);
--- src/time-utils.h
+++ src/time-utils.h
@@ -1,8 +1,10 @@
 #pragma once
 
+// A 32-bit integer for milliseconds would wrap around 25 days, so a 64-bit
+// integer is used instead, which will last more than 290 million years.
 typedef long long Milliseconds;
 
-
+// Returns the time since epoch in milliseconds.
 Milliseconds getTimeInMilliseconds();
 
 void sleepForMilliseconds(Milliseconds ms);
