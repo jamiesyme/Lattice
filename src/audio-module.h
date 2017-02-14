fa87d279314dd3574d5d8625f551eb7e6617069e
Jamie Syme <jamie.d.syme@gmail.com>
2017-02-13 17:33:59 -0800

    Noted maudio dependency for audio module.

--- src/audio-module.h
+++ src/audio-module.h
@@ -3,6 +3,9 @@
 // The audio module displays the enabled output sink (headphones or speakers),
 // as well as the volume. It also has an indicator for when the audio is muted;
 // currently, it colors the volume bar red when muted.
+// NOTE: the audio module depends on an external program called maudio to get
+// the current audio settings. The program can be found here:
+// https://github.com/jamiesyme/dotfiles/tree/master/maudio
 
 typedef struct Module Module;
 
